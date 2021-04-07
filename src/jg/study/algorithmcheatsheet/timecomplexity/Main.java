906c07e5c61826f778f69e4b9143cc44dcc74748
이재국 <irnd04@gmail.com>
2021-04-07 19:36:33 +0900

    1초가 걸리는 입력의 크기

---
+++ src/jg/study/algorithmcheatsheet/timecomplexity/Main.java
@@ -0,0 +1,17 @@
+package jg.study.algorithmcheatsheet.timecomplexity;
+
+/**
+ * 1초가 걸리는 입력의 크기
+ * O(1)
+ * O(log N)
+ * O(N) : 1억
+ * O(N log N) : 5백만
+ * O(N^2) : 1만
+ * O(N^3) : 500
+ * O(2^N) : 20
+ * O(N!) : 10
+ *
+ */
+
+public class Main {
+}
